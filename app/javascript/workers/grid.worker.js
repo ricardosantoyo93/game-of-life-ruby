bb97a64b49330b4f0dcbcdf892c2c4303fe27e3c
Ricardo Santoyo Reyes <ricardosantoyo93@yahoo.com>
2020-06-20 15:42:29 -0500

    Added grid worker

---
+++ app/javascript/workers/grid.worker.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+// Interval instance
+let interval = null;
+
+/**
+ * Takes an original grid, calculates each iteration of each cell, and then sends a message to the main thread to update the grid
+ * @param {Array<Array<Boolean>>} grid Original grid
+ */
+export const calculateNewGrid = (grid) => {
+    let baseGrid = [...grid];
+
+    interval = setInterval(async () => {
+        const res = await axios.post('/api/v1/grid', { grid: baseGrid });
+
+        baseGrid = res.data.grid;
+
+        postMessage({ method: 'update-grid', grid: baseGrid });
+    }, 350);
+}
+
+/**
+ * Clears the interval and stops the worker
+ */
+export const stopWorker = () => {
+    clearInterval(interval);
+    postMessage({ method: 'worker-stopped' });
+}
